a51ffc3f5301493fc5adcc234bb4802a2ef1968c
Dehayo <yusshayo@gmail.com>
2023-11-30 10:24:26 -0300

    deploy vercel

--- src/config/AutenticacaoGeral.js
+++ src/config/AutenticacaoGeral.js
@@ -8,9 +8,9 @@ export const AutenticacaoGeral = (props) => {
     const resposta = autenticacao.token && Array.isArray(autenticacao.roleNames) && autenticacao.roleNames.length > 0;
 
     if (!autenticacao.token || !resposta) {
-    //    toast.error("Acesso negado.", { position: "bottom-right", duration: 2000 });
-      //  navigate("/error1");
-        //return <Navigate to="/error" />;
+        toast.error("Acesso negado.", { position: "bottom-right", duration: 2000 });
+        navigate("/error");
+        return <Navigate to="/error" />;
     }
 
     return <Outlet {...props} />;
